b6a459289545dc820693e9ef9220688d71a8727b
marina-terina <marinat@outlook.com.au>
2024-09-21 12:42:33 +1000

    pledge update

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from django.apps import apps
+from .models import Project, Pledge
+
+class PledgeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pledge
+        fields = '__all__'
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
-        model = apps.get_model('projects.Project')
+        model = Project
         fields = '__all__'
+
+class ProjectDetailSerializer(ProjectSerializer):
+    pledges = PledgeSerializer(many=True, read_only=True)
